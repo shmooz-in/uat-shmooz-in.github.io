e5df3609e7d65d61f16d5ba60b06138e43e26590
Akash Agarwal <aascagarwal@gmail.com>
2024-02-29 19:39:16 +0530

    code updated

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -17,13 +17,96 @@ firebase.initializeApp(firebaseConfig);
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
 
+const formatData = (input) => {
+    if (input > 9) {
+        return input;
+    } else return `0${input}`;
+};
+
+// Function to convert
+// 24 Hour to 12 Hour clock
+const formatHour = (input) => {
+    if (input > 12) {
+        return input - 12;
+    }
+    return input;
+};
+
+const formatDate = (date) => {
+    const today = new Date(date);
+    const yyyy = today.getFullYear();
+    let mm = today.getMonth() + 1; // Months start at 0!
+    let dd = today.getDate();
+
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+
+    const formattedToday = dd + '/' + mm + '/' + yyyy;
+
+    return formattedToday;
+};
+
+const formatTime = (date) => {
+    const today = new Date(date);
+    let hh = formatData(formatHour(today.getHours()));
+    let MM = formatData(today.getMinutes());
+    const formattedToday = `${hh}:${MM} ${today.getHours() > 12 ? 'PM' : 'AM'}`;
+
+    return formattedToday;
+};
+
 messaging.onBackgroundMessage(function (payload) {
     console.log('Received background message ', payload);
     // Customize notification here
-    const notificationTitle = payload.notification.title + 'custom';
+
+    const type = payload.data.type;
+
+    const { venue_name, from_user_name, start_date, start_time, end_date, end_time, from_user_avatar } = payload.data;
+
+    let notificationTitle = '';
+
+    let body = payload.body;
+
+    switch (type) {
+        case 'venue_booking_request': {
+            notificationTitle = 'Venue Booking Request';
+            const startDate = formatDate(start_date);
+            const startTime = formatTime(`${start_date} ${start_time}`);
+
+            const endDate = formatDate(end_date);
+            const endTime = formatTime(`${end_date} ${end_time}`);
+
+            body = `${from_user_name} has requested booking for Venue (${venue_name}) from ${startDate} ${startTime} to ${endDate} ${endTime}`;
+        }
+    }
+    // = payload.notification.title + 'custom';
     const notificationOptions = {
-        body: payload.notification.body
+        body: body,
+        data: { ...payload.data, url: 'http://localhost:3000/manage/venue-list/' },
+        icon: from_user_avatar
     };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+////Code for adding event on click of notification
+self.addEventListener('notificationclick', function (event) {
+    console.log('notificationclick', event);
+    var urlToRedirect = event.notification.data.url;
+    event.notification.close();
+    //event.waitUntil(self.clients.openWindow(urlToRedirect));
+
+    event.waitUntil(
+        clients
+            .matchAll({
+                includeUncontrolled: true,
+                type: 'window'
+            })
+            .then((clientList) => {
+                for (const client of clientList) {
+                    if ('focus' in client) return client.focus();
+                }
+                if (clients.openWindow) return clients.openWindow(urlToRedirect);
+            })
+    );
 });
