1f2681edfa9085a5f7355931e27c41f914d01e0a
Akash Agarwal <aascagarwal@gmail.com>
2024-04-04 18:45:26 +0530

    changes

---
+++ static/js/3866.612069da.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkshmooz=self.webpackChunkshmooz||[]).push([[3866],{60806:function(c,e,s){s(47313);e.Z=s.p+"static/media/empty-dark.c042af2d893430e9c742f636542451c8.svg"},70268:function(c,e,s){s(47313);e.Z=s.p+"static/media/empty.6dc90e3ee832f028982c3c6983377919.svg"}}]);
