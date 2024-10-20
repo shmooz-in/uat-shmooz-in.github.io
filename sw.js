faea0cf573822f5f7999e9ac7a8c027787fdd2e9
aascagarwal <aascagarwal@gmail.com>
2024-10-20 14:14:53 +0530

    changes for chat internal notifications

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
 ////Code for adding event on click of notification
 self.addEventListener('notificationclick', function (event) {
     console.log('notificationclick', event);
+    const url = event.notification?.data?.url;
+    const serviceUrl = `${event.target.location.origin}${url}`;
 
     event.notification.close();
+
     //event.waitUntil(self.clients.openWindow(urlToRedirect));
 
     event.waitUntil(
@@ -11,11 +14,26 @@ self.addEventListener('notificationclick', function (event) {
                 includeUncontrolled: true,
                 type: 'window'
             })
-            .then((clientList) => {
-                for (const client of clientList) {
+            .then((windowClients) => {
+                let matchingClient = null;
+                for (let i = 0; i < windowClients.length; i++) {
+                    const windowClient = windowClients[i];
+                    if (windowClient.url === serviceUrl) {
+                        matchingClient = windowClient;
+                        break;
+                    }
+                }
+
+                if (matchingClient) {
+                    return matchingClient.focus();
+                } else {
+                    return clients.openWindow(serviceUrl);
+                }
+
+                /*   for (const client of clientList) {
                     if ('focus' in client) return client.focus();
                 }
-                if (clients.openWindow) return clients.openWindow('/app/chat');
+                if (clients.openWindow) return clients.openWindow(url); */
             })
     );
 });
